dad4703b409f4f0a8a72633615bfa31cf9ef125a
Hanumaraddi vaddatti <93374801+HanumaraddiBV@users.noreply.github.com>
2022-03-18 10:53:08 +0530

    added another project

--- src/modules/toggleTheme.js
+++ src/modules/toggleTheme.js
@@ -7,6 +7,7 @@ const addThemeClass = (bodyClass, btnClass) => {
 
 const isDark = () => document.body.classList.contains('body-dark');
 
+// let nav_bar = document.getElementById(top);
 const setTheme = (bodyClass, btnClass) => {
 	document.body.classList.remove(localStorage.getItem('class-body-theme'));
 	btnTheme.classList.remove(localStorage.getItem('class-btn-theme'));
@@ -14,10 +15,17 @@ const setTheme = (bodyClass, btnClass) => {
 	localStorage.setItem('class-body-theme', bodyClass);
 	localStorage.setItem('class-btn-theme', btnClass);
 };
-
 const toggleTheme = () =>
 	isDark()
 		? setTheme('body-light', 'fa-moon')
 		: setTheme('body-dark', 'fa-sun');
 
+
 export { toggleTheme, addThemeClass };
+// isDark() ? `nav_bar.style.backgroundColor="#f5f6f7"`: `nav_bar.style.backgroundColor="red"`;
+// if(isDark()){
+// 	nav_bar.style.backgroundColor="#f5f6f7"
+// } else{
+// 	nav_bar.style.backgroundColor="red"
+// }
+// i have to change nav_bar
